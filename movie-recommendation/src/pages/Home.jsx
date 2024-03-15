923bae08aa2af1e9f832c99aee797b88eb465f31
Maedot Amha <maedotamha@gmail.com>
2024-03-15 12:38:13 +0300

    change

--- movie-recommendation/src/pages/Home.jsx
+++ movie-recommendation/src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from "react";
 import Main from "../components/Main";
+import Row from "../components/Row";
+import GenreSelector from "../components/GenreSelector";
 import requests from "../Requests";
+<<<<<<< HEAD
 import GenreSelector from "..//GenreSelector";
+=======
+>>>>>>> 1223251c6d36c084a9a18d5d2f915eda955382a4
 
 const Home = () => {
   const [genres, setGenres] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchGenres = async () => {
@@ -30,8 +36,12 @@ const Home = () => {
       ) : (
         <>
           <GenreSelector genres={genres} />
-          <Main />
-          
+          <Main genre="popular" />
+          <Row title="UpComing" fetchURL={requests.requestUpcoming} rowID='1' genre="upcoming" />
+          <Row title="Popular" fetchURL={requests.requestPopular} rowID='2' genre="popular" />
+          <Row title="Top Rated" fetchURL={requests.requestTopRated} rowID='3' genre="top_rated" />
+          <Row title="Trending" fetchURL={requests.requestTrending} rowID='4' genre="popular" />
+          <Row title="Horror" fetchURL={requests.requestHorror} rowID='5' />
         </>
       )}
     </div>
