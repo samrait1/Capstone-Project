5763ce0e8c6bbc2a88d2c2b9c9f71d0ee7e01f24
salemhabte <habtesalem20@gmail.com>
2024-03-15 15:57:16 -0700

    change

--- movie-recommendation/src/components/Footer.jsx
+++ movie-recommendation/src/components/Footer.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import './foot.css'
+import telegarm from '../images/telegram.png'
+import Instagram from '../images/instagram.png'
+import email from '../images/email.png'
+import linkedin from '../images/linkedin.png'
 const Footer = () => {
   return (
     <footer className="footer">
          <h3>Contact Us:</h3>
       <ul className='ul'>
          
-          <li><a href="#" className='fo'>Email</a></li>
-          <li><a href="#" className='fo'>linkedin</a></li>
-          <li><a href="#" className='fo'>Instagram</a></li>
-          <li><a href="#" className='fo'>Telegram</a></li>
+          <li><img src={email} alt="" /><a href="#" className='fo'>Email</a></li>
+          <li><img src={linkedin} alt="" /><a href="#" className='fo'>linkedin</a></li>
+          <li><img src={Instagram} alt="" /><a href="#" className='fo'>Instagram</a></li>
+          <li><img src={telegarm} alt="" /><a href="#" className='fo'>Telegram</a></li>
         </ul>
         <h6>Copyright &copy 20204 LinkE.com</h6>
     </footer>
